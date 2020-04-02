922e65fdec5b3798f8364d9a85773464de76dcdb
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-01 23:23:06 -0700

    get all quotes route test

--- __tests__/quotes.test.js
+++ __tests__/quotes.test.js
@@ -25,7 +25,7 @@ describe('routes for QUOTE model test', () => {
       name: 'Bob',
       image: 'hereisimage.png' })));
     quote = JSON.parse(JSON.stringify(await Quote.create({
-      character: character.id,
+      character: character._id,
       body: '"This is a real Twin Peaks quote that I, me, Bob, said."'
     })));
   });
@@ -47,6 +47,19 @@ describe('routes for QUOTE model test', () => {
           body: '"My second real quote, by me, Bob, from Twin Peaks."',
           _id: expect.any(String)
         });
+      });
+  });
+
+  it('can GET all QUOTES', () => {
+    return request(app)
+      .get('/api/v1/quotes')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: quote._id,
+          character: quote.character,
+          /* eslint-disable-next-line */
+          body: `"This is a real Twin Peaks quote that I, me, Bob, said."`
+        }]);
       });
   });
 
