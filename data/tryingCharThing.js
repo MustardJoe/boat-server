f08251cc9753098da775ea528bad7f38c51e2163
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-09 23:40:43 -0700

    oh you helpful file

--- data/tryingCharThing.js
+++ data/tryingCharThing.js
@@ -1,5 +1,69 @@
-const chars = {
-  
-};
+const chars = [
+//   {
+//     name: 'Annie Blackburn',
+//     image: 'https://vignette.wikia.nocookie.net/twinpeaks/images/1/14/Annie_Blackburn.jpg/revision/latest?cb=20190921214308'
+//   },
+//   {
+//     name: 'Agent Denise Bryson',
+//     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-r8_Xi4SW7WE%2FWMAXsu44jZI%2FAAAAAAAAkbU%2FLTFuympNK183ydPFx8KBqw_C1u_5xJ8UwCLcB%2Fs640%2FPicture%252B6.png&f=1&nofb=1',
+//   },
+// {
+//     name: 'Deputy Director Gordon Cole',
+//     image: 'https://vignette.wikia.nocookie.net/twinpeaks/images/7/7a/GordonColeNewMainPhoto.png/revision/latest?cb=20171109180605'
+//   },
+// {
+//     name: 'Agent Dale Cooper',
+//     image: 'https://cnet4.cbsistatic.com/img/VHWr511OOJsjcc8EjgQIv8uldRQ=/1092x0/2014/10/03/26bc6e31-8e32-46ed-aa6b-0a6784b9f722/thumbsup.jpg'
+//   },
+// {
+//     name: 'Deputy Hawk',
+//     image: 'https://m.media-amazon.com/images/M/MV5BMTkzNDk1ODc4NV5BMl5BanBnXkFtZTgwMTAyMjg0MDE@._V1_.jpg'
+//   },
+{
+    name: 'Donna Hayward',
+    image: 'https://i.pinimg.com/originals/dd/26/88/dd2688d50d8641f2db6b48b0f70e0fab.jpg'
+  },
+{
+    name: 'Dr. Will Hayward',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpmctvline2.files.wordpress.com%2F2017%2F02%2Fwarren-frost-dead-dies-twin-peaks-seinfeld-matlock.jpg%3Fw%3D620%26h%3D420%26crop%3D1&f=1&nofb=1'
+  },
+{
+    name: 'Audry Horne',
+    image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-xmoZtnB9fxw%2FUUdmvYDH65I%2FAAAAAAAAATA%2FHJz1ERltktw%2Fs1600%2FAudrey%2BHorne.jpg&f=1&nofb=1'
+  },
+{
+    name: 'James Hurley',
+    image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.cheatsheet.com%2Fwp-content%2Fuploads%2F2016%2F09%2Fjames-hurley-twin-peaks-640x426.jpg&f=1&nofb=1'
+  },
+{
+    name: 'Norma Jennings',
+    image: 'https://www.thewrap.com/wp-content/uploads/2017/05/norma-jennings-twin-peaks-300x200.jpg'
+  },
+{
+    name: 'Shelly Johnson',
+    image: 'https://i.pinimg.com/originals/0c/0b/f4/0c0bf45f695914a193ee452887b3f296.png'
+  },
+{
+    name: 'Log Lady',
+    image: 'https://vignette.wikia.nocookie.net/twinpeaks/images/4/4f/Shot0003.png/revision/latest?cb=20140210120651'
+  },
+{
+    name: 'Pete Martell',
+    image: 'https://vignette.wikia.nocookie.net/twinpeaks/images/c/c7/X_2574f1b8.jpg/revision/latest/scale-to-width-down/340?cb=20111126081930'
+  },
+ {
+    name: 'Mayor Milford',
+    image: 'https://4.bp.blogspot.com/-KUnc0IcWWNw/WNL5DiaWk3I/AAAAAAAAk6s/JU9Lkkd6jisjv_IzEnORPBkUosZrH3x9gCLcB/s1600/Picture%2B43.png'
+  },
+{
+    name: 'Agent Albert Rosenfield',
+    image: 'https://3.bp.blogspot.com/-H1iA9zUc25k/WPYdMiFAqxI/AAAAAAAAmIQ/hQYc3B1fK1kmHEDYLfGtA2Upe5Ib9ScHQCEw/s1600/Picture%2B21.png'
+  },
+ {
+    name: 'Sheriff Harry S. Truman',
+    image: 'https://heavyeditorial.files.wordpress.com/2017/05/sheriff-truman-where.jpg?quality=65&strip=all&w=780'
+  }
+
+];
 
 console.log(JSON.stringify(chars));
