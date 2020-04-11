bd76d1ed48b39411f4b3c86d3cbb69e54b9f5a27
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-10 20:15:30 -0700

    trying to figure out how to import data and have it preserve relationships

--- data/tryingCharThing.js
+++ data/tryingCharThing.js
@@ -1,63 +1,41 @@
+/*eslint quotes: ["error", "double", { "avoidEscape": true }]*/
+
 const chars = [
   {
-    character: ['5e900e37cfcfaa003648b5d9', '5e900f73cfcfaa003648b5e4'],
-    body: `Cooper: "I'll talk to her.  Maybe she'll confess. Turn herself in."
-    Rosenfield: "Maybe she'll grow wings and join the circus."`
+    character: ["5e900f73cfcfaa003648b5df", "5e9009aacfcfaa003648b5d4"],
+    body: "Bobby: 'Norma, I'll see you in my dreams.' Norma: 'Not if I see you first.'"
   },
   {
-    character: ['5e900f73cfcfaa003648b5df', '5e9009aacfcfaa003648b5d4'],
-    body: `Bobby: "Norma, I'll see you in my dreams."
-    Norma: "Not if I see you first."`
+    character: ["5e900f73cfcfaa003648b5db"],
+    body: "I believe you. I applaud your desire to do the right thing. But goodness in you is like a timebomb. And there's nothing good about ruined lives."
   },
   {
-    character: ['5e900f73cfcfaa003648b5db'],
-    body: `"I believe you. I applaud your desire to do the right thing. But 
-    goodness in you is like a timebomb. And there's nothing good about 
-    ruined lives."`
+    character: ["5e900f73cfcfaa003648b5e2", "5e900e37cfcfaa003648b5d9"],
+    body: "Pete: 'Mr. Cooper, how do you take it?' Cooper: 'Black as midnight on a moonless night.'"
   },
   {
-    character: ['5e900f73cfcfaa003648b5e2', '5e900e37cfcfaa003648b5d9'],
-    body: `Pete: "Mr. Cooper, how do you take it?"
-    Cooper: "Black as midnight on a moonless night."`
+    character:["5e900f73cfcfaa003648b5e1"],
+    body: "And now, an ending. Where there was once one, there are now two. Or were there always two? What is a reflection? A chance to see two? When there are chances for reflections, there can always be two — or more. Only when we are everywhere will there be just one"
   },
   {
-    character:['5e900f73cfcfaa003648b5e1'],
-    body: `"And now, an ending. Where there was once one, there are
-    now two. Or were there always two? What is a reflection? A
-    chance to see two? When there are chances for reflections, there
-    can always be two — or more. Only when we are everywhere will
-    there be just one"`
+    character: ["5e900e37cfcfaa003648b5d9"],
+    body: "If we're not at the right place at exactly the right time, we won't find our way in."
   },
   {
-    character: ['5e900e37cfcfaa003648b5d9'],
-    body: `"If we're not at the right place at exactly the right time,
-    we won't find our way in."`
+    character: ["5e900e37cfcfaa003648b5d8"],
+    body: "If I don't kiss you now, I'm gonna regret it the rest of my days."
   },
   {
-    character: ['5e900e37cfcfaa003648b5d8'],
-    body: `"If I don't kiss you now, I'm gonna regret it the rest
-    of my days."`
+    character: ["5e900f73cfcfaa003648b5e5"],
+    body: "I'd never heard of a man who murdered by the rules."
   },
   {
-    character: ['5e900f73cfcfaa003648b5e5'],
-    body: `"I'd never heard of a man who murdered by the rules."`
+    character: ["5e900f73cfcfaa003648b5e1"],
+    body: "The beautiful thing about treasure is that it exists. It exists to be found. How beautiful it is to find treasure. Where is the treasure, that when found, leaves one eternally happy? I think we all know it exists. Some say it is inside us — inside us one and all. That would be strange. It would be so near. Then why is it so hard to find, and so difficult to attain?"
   },
   {
-    character: ['5e900f73cfcfaa003648b5e1'],
-    body: `"The beautiful thing about treasure is that it exists. It
-    exists to be found. How beautiful it is to find treasure. Where
-    is the treasure, that when found, leaves one eternally happy? I
-    think we all know it exists. Some say it is inside us — inside
-    us one and all. That would be strange. It would be so near. Then
-    why is it so hard to find, and so difficult to attain?"`
-  },
-  {
-    character: ['5e900f73cfcfaa003648b5e1'],
-    body: `"How do you feel about yourself? Are you proud of your
-    behavior? Are you ashamed of your behavior? You know in your
-    heart if you have hurt someone — you know. If you have hurt
-    someone, don't wait another day before making things right. The
-    world could break apart with sadness in the meantime."`
+    character: ["5e900f73cfcfaa003648b5e1"],
+    body: "How do you feel about yourself? Are you proud of your behavior? Are you ashamed of your behavior? You know in your heart if you have hurt someone — you know. If you have hurt someone, don't wait another day before making things right. The world could break apart with sadness in the meantime."
   }
 ];
 
