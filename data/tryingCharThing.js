dee65a76633ea74605972b15d17fb2d4a9960812
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-11 23:40:20 -0700

    got next 20 data bits formatted

--- data/tryingCharThing.js
+++ data/tryingCharThing.js
@@ -2,40 +2,80 @@
 
 const chars = [
   {
-    character: ['5e900f73cfcfaa003648b5df', '5e9009aacfcfaa003648b5d4'],
-    body: "Bobby: 'Norma, I'll see you in my dreams.' Norma: 'Not if I see you first.'"
+    "character": ["5e900f73cfcfaa003648b5e1"],
+    "body": "Is life like a game of chess? Are our present moves important for future success? I think so. We paint our future with every present brush stroke. Painting. Colors. Shapes. Textures. Composition. Repetition of shapes. Contrast. Let nature guide us. Nature is the great teacher."
   },
   {
-    character: ["5e900f73cfcfaa003648b5db"],
-    body: "I believe you. I applaud your desire to do the right thing. But goodness in you is like a timebomb. And there's nothing good about ruined lives."
+    "character": ["5e900f73cfcfaa003648b5e1"],
+    "body": "How many times have we heard: 'it's simple'. Nothing is simple. We live in a world where nothing is simple. Each day, just when we think we have a handle on things, suddenly some new element is introduced and everything is complicated once again."
   },
   {
-    character: ["5e900f73cfcfaa003648b5e2", "5e900e37cfcfaa003648b5d9"],
-    body: "Pete: 'Mr. Cooper, how do you take it?' Cooper: 'Black as midnight on a moonless night.'"
+    "character": ["5e900e37cfcfaa003648b5d9"],
+    "body": "There's nothing quite like urinating in the open air."
   },
   {
-    character:["5e900f73cfcfaa003648b5e1"],
-    body: "And now, an ending. Where there was once one, there are now two. Or were there always two? What is a reflection? A chance to see two? When there are chances for reflections, there can always be two — or more. Only when we are everywhere will there be just one"
+    "character": ["5e900f73cfcfaa003648b5e1"],
+    "body": "Food is interesting. For instance, why do we need to eat? Why are we never satisfied with just the right amount of food to maintain good health and proper energy? We always seem to want more and more. When eating too much, the proper balance is disturbed and ill health follows. Of course, eating too little food throws the balance off in the opposite direction and there is the ill health coming at us again. Balance is the key. Balance is the key to many things. Do we understand balance? The word 'balance' has seven letters. Seven is difficult to balance, but not impossible if we are able to divide. There are, of course, the pros and cons of division."
   },
   {
-    character: ["5e900e37cfcfaa003648b5d9"],
-    body: "If we're not at the right place at exactly the right time, we won't find our way in."
+    "character": ["5e900f73cfcfaa003648b5e1"],
+    "body": "Yes, look in the mirror. What do you see? Is it a dream, or a nightmare? Are we being introduced against our will? Are they mirrors?"
   },
   {
-    character: ["5e900e37cfcfaa003648b5d8"],
-    body: "If I don't kiss you now, I'm gonna regret it the rest of my days."
+    "character": ["5e900f73cfcfaa003648b5e0"],
+    "body": "Leo doesn't talk, he hits. He was so great at first, you know. This flashy guy in his hot car. Then we get married and I find out all he was looking for was a maid he didn't have to pay. I feel so stupid."
   },
   {
-    character: ["5e900f73cfcfaa003648b5e5"],
-    body: "I'd never heard of a man who murdered by the rules."
+    "character": ["5e900f73cfcfaa003648b5e5"],
+    "body": "There's a sort of evil out there. Something very, very strange in these old woods. Call it what you want. A darkness, a presence. It takes many forms but... its been out there for as long as anyone can remember and we've always been here to fight it."
   },
   {
-    character: ["5e900f73cfcfaa003648b5e1"],
-    body: "The beautiful thing about treasure is that it exists. It exists to be found. How beautiful it is to find treasure. Where is the treasure, that when found, leaves one eternally happy? I think we all know it exists. Some say it is inside us — inside us one and all. That would be strange. It would be so near. Then why is it so hard to find, and so difficult to attain?"
+    "character": ["5e900e37cfcfaa003648b5d9"],
+    "body": "Nothing beats the taste sensation when maple syrup collides with ham."
   },
   {
-    character: ["5e900f73cfcfaa003648b5e1"],
-    body: "How do you feel about yourself? Are you proud of your behavior? Are you ashamed of your behavior? You know in your heart if you have hurt someone — you know. If you have hurt someone, don't wait another day before making things right. The world could break apart with sadness in the meantime."
+    "character": ["5e900f73cfcfaa003648b5e1"],
+    "body": "There is a sadness in this world, for we are ignorant of many things. Yes, we are ignorant of many beautiful things — things like the truth. So sadness, in our ignorance, is very real. The tears are real. What is this thing called a tear? There are even tiny ducts — tear ducts — to produce these tears should the sadness occur. Then the day when the sadness comes — then we ask: 'Will this sadness which makes me cry — will this sadness that makes me cry my heart out — will it ever end?' The answer, of course, is yes. One day the sadness will end."
+  },
+  {
+    "character": ["5e900f73cfcfaa003648b5e1"],
+    "body": "Sometime ideas, like men, jump up and say 'hello'. They introduce themselves, these ideas, with words. Are they words? These ideas speak so strangely. All that we see in this world is based on someone's ideas. Some ideas are destructive, some are constructive. Some ideas can arrive in the form of a dream. I can say it again: some ideas arrive in the form of a dream."
+  },
+  {
+    "character": ["5e900f73cfcfaa003648b5e1"],
+    "body": "Behind all things are reasons. Reasons can even explain the absurd. Do we have the time to learn the reasons behind the human being's varied behavior? I think not. Some take time."
+  },
+  {
+    "character": ["5e900f73cfcfaa003648b5e5", "5e900e37cfcfaa003648b5d9"],
+    "body": "Sheriff Truman: 'You know, I think I'd better start studying medicine.' Dale Cooper: 'And why is that?' Sheriff Truman: 'Because I'm beginning to feel a bit like Dr. Watson.'"
+  },
+  {
+    "character": ["5e900e37cfcfaa003648b5d9"],
+    "body": "Now, I'd like two eggs, over hard. I know, don't tell me; it's hard on the arteries, but old habits die hard — just about as hard as I want those eggs."
+  },
+  {
+    "character": ["5e900f73cfcfaa003648b5e5", "5e900e37cfcfaa003648b5d9"],
+    "body": "Dale Cooper: 'Who's the lady with the log?' Sheriff Truman: 'We call her the Log Lady.'"
+  },
+  {
+    "character": ["5e900f73cfcfaa003648b5e4", "5e900e37cfcfaa003648b5d8"],
+    "body": "Albert Rosenfield: 'I said, you've gone soft in your old age.' Gordon Cole: 'Not where it counts buddy.'"
+  },
+  {
+    "character": ["5e900e37cfcfaa003648b5d9"],
+    "body": "You know, this is — excuse me — a damn fine cup of coffee!"
+  },
+  {
+    "character": ["5e900f73cfcfaa003648b5e1"],
+    "body": "Welcome to Twin Peaks. My name is Margaret Lanterman. I live in Twin Peaks. I am known as the Log Lady. There is a story behind that. There are many stories in Twin Peaks — some of them are sad, some funny. Some of them are stories of madness, of violence. Some are ordinary. Yet they all have about them a sense of mystery — the mystery of life. Sometimes, the mystery of death. The mystery of the woods. The woods surrounding Twin Peaks. To introduce this story, let me just say it encompasses the All — it is beyond the 'Fire,' though few would know that meaning. It is a story of many, but begins with one — and I knew her. The one leading to the many is Laura Palmer. Laura is the one."
+  },
+  {
+    "character": ["5e900e37cfcfaa003648b5d9"],
+    "body": "I've got a joke for you. Two penguins were walking across an iceberg. One penguin turned to the other penguin and said, 'You look like you are wearing a tuxedo.'"
+  },
+  {
+    "character": ["5e900e37cfcfaa003648b5d9"],
+    "body": "Harry, I’m going to let you in on a little secret. Every day, once a day, give yourself a present. Don’t plan it, don’t wait for it, just let it happen."
   }
 ];
 
