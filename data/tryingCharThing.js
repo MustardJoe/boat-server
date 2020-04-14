f1685956a0941215e9f06e08dab468f79ae6843e
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-13 18:54:58 -0700

    🏉next 20 quotes added to db

--- data/tryingCharThing.js
+++ data/tryingCharThing.js
@@ -2,80 +2,84 @@
 
 const chars = [
   {
-    "character": ["5e900f73cfcfaa003648b5e1"],
-    "body": "Is life like a game of chess? Are our present moves important for future success? I think so. We paint our future with every present brush stroke. Painting. Colors. Shapes. Textures. Composition. Repetition of shapes. Contrast. Let nature guide us. Nature is the great teacher."
+    "character": ["5e900e37cfcfaa003648b5d9", "5e900f73cfcfaa003648b5e5"],
+    "body": "Harry: 'You know there are plenty of other vets in Twin Peaks.' Dale Cooper: 'Yes but only one has a best friend with one arm. Harry, in the heat of the investigative pursuit, the shortest distance between two points is not necessarily a straight line.'"
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e1"],
-    "body": "How many times have we heard: 'it's simple'. Nothing is simple. We live in a world where nothing is simple. Each day, just when we think we have a handle on things, suddenly some new element is introduced and everything is complicated once again."
+    "character": ["5e900f73cfcfaa003648b5e2"],
+    "body": "Fellas don't drink that coffee! You'd never guess, there was a fish... in the percolator. Sorry."
   },
   {
-    "character": ["5e900e37cfcfaa003648b5d9"],
-    "body": "There's nothing quite like urinating in the open air."
+    "character": ["5e8fb51f959b3e0017376fa0"],
+    "body": "Tell Harry… I didn’t cry. But Lucy, it’s so horrible."
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e1"],
-    "body": "Food is interesting. For instance, why do we need to eat? Why are we never satisfied with just the right amount of food to maintain good health and proper energy? We always seem to want more and more. When eating too much, the proper balance is disturbed and ill health follows. Of course, eating too little food throws the balance off in the opposite direction and there is the ill health coming at us again. Balance is the key. Balance is the key to many things. Do we understand balance? The word 'balance' has seven letters. Seven is difficult to balance, but not impossible if we are able to divide. There are, of course, the pros and cons of division."
+    "character": ["5e8fb51f959b3e0017376fa0"],
+    "body": "Albert Roserfeld. I don’t like the way you talk smart about Sheriff Truman or anybody. You just shut your mouth!"
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e1"],
-    "body": "Yes, look in the mirror. What do you see? Is it a dream, or a nightmare? Are we being introduced against our will? Are they mirrors?"
+    "character": ["5e8fb51f959b3e0017376fa0"],
+    "body": "When the Tacoma Sperm Bank was looking for donors, naturally I applied. It’s my civic duty and I like whales."
+  },
+  {
+    "character": ["5e8fb51f959b3e0017376fa0"],
+    "body": "Tremaine, Brennan, Andy Brennan! We need to talk… That is if you are not too busy."
+  },
+  {
+    "character": ["5e900f73cfcfaa003648b5dd"],
+    "body": "You know, sometimes I get so flushed, it’s interesting. Do your palms ever itch?"
+  },
+  {
+    "character": ["5e900f73cfcfaa003648b5dd"],
+    "body": "I’m Audrey Horne and I get what I want."
   },
   {
     "character": ["5e900f73cfcfaa003648b5e0"],
-    "body": "Leo doesn't talk, he hits. He was so great at first, you know. This flashy guy in his hot car. Then we get married and I find out all he was looking for was a maid he didn't have to pay. I feel so stupid."
+    "body": "I’ve got one man too many in my life and I’m married to him."
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e5"],
-    "body": "There's a sort of evil out there. Something very, very strange in these old woods. Call it what you want. A darkness, a presence. It takes many forms but... its been out there for as long as anyone can remember and we've always been here to fight it."
+    "character": ["5e900f73cfcfaa003648b5e0"],
+    "body": "I’m a waitress in a diner. I’ve never been compared to a goddess before."
   },
   {
-    "character": ["5e900e37cfcfaa003648b5d9"],
-    "body": "Nothing beats the taste sensation when maple syrup collides with ham."
+    "character": ["5e900f73cfcfaa003648b5e0"],
+    "body": "He moved! This is too creepy. I don’t like this."
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e1"],
-    "body": "There is a sadness in this world, for we are ignorant of many things. Yes, we are ignorant of many beautiful things — things like the truth. So sadness, in our ignorance, is very real. The tears are real. What is this thing called a tear? There are even tiny ducts — tear ducts — to produce these tears should the sadness occur. Then the day when the sadness comes — then we ask: 'Will this sadness which makes me cry — will this sadness that makes me cry my heart out — will it ever end?' The answer, of course, is yes. One day the sadness will end."
+    "character": ["5e9009aacfcfaa003648b5d4"],
+    "body": "Don’t do anything I wouldn’t do."
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e1"],
-    "body": "Sometime ideas, like men, jump up and say 'hello'. They introduce themselves, these ideas, with words. Are they words? These ideas speak so strangely. All that we see in this world is based on someone's ideas. Some ideas are destructive, some are constructive. Some ideas can arrive in the form of a dream. I can say it again: some ideas arrive in the form of a dream."
+    "character": ["5e9009aacfcfaa003648b5d4"],
+    "body": "If we tell the truth we don’t have to get our stories straight."
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e1"],
-    "body": "Behind all things are reasons. Reasons can even explain the absurd. Do we have the time to learn the reasons behind the human being's varied behavior? I think not. Some take time."
+    "character": ["5e9009aacfcfaa003648b5d4"],
+    "body": "Too bad we can only kill him once."
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e5", "5e900e37cfcfaa003648b5d9"],
-    "body": "Sheriff Truman: 'You know, I think I'd better start studying medicine.' Dale Cooper: 'And why is that?' Sheriff Truman: 'Because I'm beginning to feel a bit like Dr. Watson.'"
+    "character": ["5e9009aacfcfaa003648b5d4"],
+    "body": "She wanted so much. She made me sell drugs so she could have them."
   },
   {
-    "character": ["5e900e37cfcfaa003648b5d9"],
-    "body": "Now, I'd like two eggs, over hard. I know, don't tell me; it's hard on the arteries, but old habits die hard — just about as hard as I want those eggs."
+    "character": ["5e9009aacfcfaa003648b5d4"],
+    "body": "You damn hypocrites. You make me sick! Everybody knew she was in trouble but we didn’t do anything. All you good people… You wanna know who killed Laura Palmer? You did! We all did."
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e5", "5e900e37cfcfaa003648b5d9"],
-    "body": "Dale Cooper: 'Who's the lady with the log?' Sheriff Truman: 'We call her the Log Lady.'"
+    "character": ["5e9009aacfcfaa003648b5d4"],
+    "body": "Doc Hayward said you needed familiar stimulants, so we figured, what the hell, kazoos."
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e4", "5e900e37cfcfaa003648b5d8"],
-    "body": "Albert Rosenfield: 'I said, you've gone soft in your old age.' Gordon Cole: 'Not where it counts buddy.'"
+    "character": ["5e900f73cfcfaa003648b5e2"],
+    "body": "The lonesome foghorn blows."
   },
   {
-    "character": ["5e900e37cfcfaa003648b5d9"],
-    "body": "You know, this is — excuse me — a damn fine cup of coffee!"
+    "character": ["5e900f73cfcfaa003648b5e2"],
+    "body": "You don’t like musicals? Not even Fiddler on a roof?"
   },
   {
-    "character": ["5e900f73cfcfaa003648b5e1"],
-    "body": "Welcome to Twin Peaks. My name is Margaret Lanterman. I live in Twin Peaks. I am known as the Log Lady. There is a story behind that. There are many stories in Twin Peaks — some of them are sad, some funny. Some of them are stories of madness, of violence. Some are ordinary. Yet they all have about them a sense of mystery — the mystery of life. Sometimes, the mystery of death. The mystery of the woods. The woods surrounding Twin Peaks. To introduce this story, let me just say it encompasses the All — it is beyond the 'Fire,' though few would know that meaning. It is a story of many, but begins with one — and I knew her. The one leading to the many is Laura Palmer. Laura is the one."
-  },
-  {
-    "character": ["5e900e37cfcfaa003648b5d9"],
-    "body": "I've got a joke for you. Two penguins were walking across an iceberg. One penguin turned to the other penguin and said, 'You look like you are wearing a tuxedo.'"
-  },
-  {
-    "character": ["5e900e37cfcfaa003648b5d9"],
-    "body": "Harry, I’m going to let you in on a little secret. Every day, once a day, give yourself a present. Don’t plan it, don’t wait for it, just let it happen."
+    "character": ["5e900e37cfcfaa003648b5da"],
+    "body": "Body and spirit are still far apart."
   }
 ];
 
