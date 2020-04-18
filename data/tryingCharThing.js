1b715b691a7cb8d7baa1c9c9207d0154c4446732
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-17 18:22:53 -0700

    data read to post

--- data/tryingCharThing.js
+++ data/tryingCharThing.js
@@ -1,79 +1,61 @@
 /*eslint quotes: ["error", "double", { "avoidEscape": true }]*/
 
 const chars = [
-  61: {
-    character: ['Deputy Hawk'],
-    body: `"At ten o’clock there was a one-armed man snooping
-    around intensive care."`
+  {
+    "character": ["5e900e37cfcfaa003648b5da"],
+    "body": "At ten o’clock there was a one-armed man snooping around intensive care."
   },
-  62: {
-    character: ['Deputy Hawk'],
-    body: `"Laura’s in the ground, Agent Cooper. That’s all I
-    know for sure."`
+  {
+    "character": ["5e900e37cfcfaa003648b5da"],
+    "body": "Laura’s in the ground, Agent Cooper. That’s all I know for sure."
   },
-  63: {
-    character: ['Major Garland Briggs'],
-    /* eslint-disable-next-line */
-    body: `"Achievement is it’s own reward. Pride obscures it."`
+  {
+    "character": ["5e9009aacfcfaa003648b5d5"],
+    "body": "Achievement is it’s own reward. Pride obscures it."
   },
-  64: {
-    character: ['Major Garland Briggs'],
-    body: `"This secrecy pains me from time to time. Any bureaucracy
-    that functions in secret inevitedly lends itself to corruption."`
+  {
+    "character": ["5e9009aacfcfaa003648b5d5"],
+    "body": "This secrecy pains me from time to time. Any bureaucracy that functions in secret inevitedly lends itself to corruption."
   },
-  65: {
-    character: ['Major Garland Briggs'],
-    body: `"The readout took us by surprise. Row after row of
-    gibberish and all of a sudden… The owls are not what they seem."`
+  {
+    "character": ["5e9009aacfcfaa003648b5d5"],
+    "body": "The readout took us by surprise. Row after row of gibberish and all of a sudden… The owls are not what they seem."
   },
-  66: {
-    character: ['Major Garland Briggs'],
-    body: `"My vision ended and I awoke with a tremendous feeling of
-    optimism and confidence in you and your future. That was my
-    vision of you. I’m so glad to have had this opportunity to share
-    it with you."`
+  {
+    "character": ["5e9009aacfcfaa003648b5d5"],
+    "body": "My vision ended and I awoke with a tremendous feeling of optimism and confidence in you and your future. That was my vision of you. I’m so glad to have had this opportunity to share it with you."
   },
-  67: {
-    character: ['Major Garland Briggs'],
-    body: `"There are powerful sources of evil. It is some men’s
-    fate to face great darkness."`
+  {
+    "character": ["5e9009aacfcfaa003648b5d5"],
+    "body": "There are powerful sources of evil. It is some men’s fate to face great darkness."
   },
-  68: {
-    character: ['Major Garland Briggs'],
-    body: `"We each choose how to react. If the choice is fear,
-    then we become vulnerable to darkness."`
+  {
+    "character": ["5e9009aacfcfaa003648b5d5"],
+    "body": "We each choose how to react. If the choice is fear, then we become vulnerable to darkness."
   },
-  69: {
-    character: ['Major Garland Briggs'],
-    /* eslint-disable-next-line */
-    body: `"Have you ever heard of the White Lodge?"`
+  {
+    "character": ["5e9009aacfcfaa003648b5d5"],
+    "body": "Have you ever heard of the White Lodge?"
   },
-  70: {
-    character: ['Donna Hayward'],
-    body: `"It’s like I’m having the most beautiful dream;
-    and the most terrible nightmare at once."`
+  {
+    "character": ["5e900f73cfcfaa003648b5db"],
+    "body": "It’s like I’m having the most beautiful dream; and the most terrible nightmare at once."
   },
-  71: {
-    character: ['Donna Hayward'],
-    body: `"As much as I love you, Laura, most of the time
-    we were trying to solve your problems. And you know
-    what? We still are. Not mine or James’ or Maddy’s.
-    Yours. You’re dead Laura, but your problems keep
-    hanging around!"`
+  {
+    "character": ["5e900f73cfcfaa003648b5db"],
+    "body": "As much as I love you, Laura, most of the time we were trying to solve your problems. And you know what? We still are. Not mine or James’ or Maddy’s. Yours. You’re dead Laura, but your problems keep hanging around!"
   },
-  72: {
-    character: ['Dr. Will Hayward'],
-    body: `"Donna, I’m sure you understand what you’ve put
-    your Mother and I through tonight."`
+  {
+    "character": ["5e900f73cfcfaa003648b5dc"],
+    "body": "Donna, I’m sure you understand what you’ve put your Mother and I through tonight."
   },
-  73: {
-    character: ['Dr. Will Hayward'],
-    body: `"I don’t believe I know your parents, James."`
+  {
+    "character": ["5e900f73cfcfaa003648b5dc"],
+    "body": "I don’t believe I know your parents, James."
   },
-  74: {
-    character: ['Dr. Will Hayward'],
-    body: `"I have never in my life met a man with so
-    little regard for human frailty. Have you no compassion?!"`
+  {
+    "character": ["5e900f73cfcfaa003648b5dc"],
+    "body": "I have never in my life met a man with so little regard for human frailty. Have you no compassion?!"
   },
 ];
 
