6583229e53a3408dc085e5d8c0b8236dcc2ea6df
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-19 22:15:40 -0700

    just need to swap in ids

--- data/tryingCharThing.js
+++ data/tryingCharThing.js
@@ -3,103 +3,95 @@
 const chars = [
   {
     "character": ['Lucy Moran'],
-    "body": "Sheriff, I’m going to transfer it to the phone on the table by thered chair. The red chair against the wall. The little table with lamp on it. The lamp we moved from the corner? The black phone, not the brown phone."
+    "body": "Sheriff, I’m going to transfer it to the phone on the table by the red chair. The red chair against the wall. The little table with lamp on it. The lamp we moved from the corner? The black phone, not the brown phone."
   },
   {
     "character": ['Lucy Moran'],
-    "body": `"Would you care to leave a message?"`
+    "body": "Would you care to leave a message?"
   },
   {
     "character": ['Lucy Moran'],
-    "body": `"Officer Andy, this is peak activity time at the station switch
-    board. So it’s really not convenient for me to talk right now."`
+    "body": "Officer Andy, this is peak activity time at the station switch board. So it’s really not convenient for me to talk right now."
   },
   {
     "character": ['Lucy Moran'],
-    "body": `"Birdy, birdy birdy… Want some orange? Come’on, let’s share."`
+    "body": "Birdy, birdy birdy… Want some orange? Come’on, let’s share."
   },
   {
     "character": ['Lucy Moran'],
-    "body": `"Sheriff, Leo Johnson called. He left this message. And sheriff,
-    in the background when I spoke to them, I could hear a clock striking… It sounded like the clock at Easter Park."`
+    "body": "Sheriff, Leo Johnson called. He left this message. And sheriff, in the background when I spoke to them, I could hear a clock striking… It sounded like the clock at Easter Park."
   },
   {
     "character": ['Lucy Moran'],
-    "body": `"He had lots of coats and keeps himself and his car in great shape. Most his
-    behavior was asinine but at least he was different."`
+    "body": "He had lots of coats and keeps himself and his car in great shape. Most his behavior was asinine but at least he was different."
   },
   {
     "character": ['Lucy Moran'],
-    "body": `"Richard, here’s what you’re going to do. Take your money, put it back in your
-    wallet, or your pocket, turn around, walk through both sets of doors, the second one sticks sometimes, go out into the parking lot, get in your car, turn the key, and never, ever speak to me again as long as you live! Say one more word and I’ll scream. Leave, please."`
+    "body": "Richard, here’s what you’re going to do. Take your money, put it back in your wallet, or your pocket, turn around, walk through both sets of doors, the second one sticks sometimes, go out into the parking lot, get in your car, turn the key, and never, ever speak to me again as long as you live! Say one more word and I’ll scream. Leave, please."
   },
   {
     "character": ['Deputy Hawk'],
-    "body": `"Some of my best friends are white people."`
+    "body": "Some of my best friends are white people."
   },
   {
     "character": ['Leland Palmer'],
-    "body": `"Leland, Leland, you’ve been a good vehicle and I’ve enjoyed the ride. But now he’s weak and full of holes. It’s almost time to shuffle off to Buffalo!"`
+    "body": "Leland, Leland, you’ve been a good vehicle and I’ve enjoyed the ride. But now he’s weak and full of holes. It’s almost time to shuffle off to Buffalo!"
   },
   {
     "character": ['Leo Johnson'],
-    "body": `"Shelly, sit down here a minute and help me out."`
+    "body": "Shelly, sit down here a minute and help me out."
   },
   {
     "character": ['Leland Palmer'],
-    "body": `"We have to dance for Laura!"`
+    "body": "We have to dance for Laura!"
   },
   {
     "character": ['Ben Horne'],
-    "body": `"Admiration is for poets and dairy cows, Bobby."`
+    "body": "Admiration is for poets and dairy cows, Bobby."
   },
   {
     "character": ['Josie Packard'],
-    "body": `"On top of the morning to you, Pete."`
+    "body": "On top of the morning to you, Pete."
   },
   {
     "character": ['Dr. Jacoby'],
-    "body": `"Laura had secrets and around those secrets she built the fortress that,
-    well, that in my six months with her, I was not able to penetrate, and for which I consider myself an abject failure. "`
+    "body": "Laura had secrets and around those secrets she built the fortress that, well, that in my six months with her, I was not able to penetrate, and for which I consider myself an abject failure. "
   },
   {
     "character": ['Richard Tremayne'],
-    "body": `"Honestly, I barely have the time to feed my cat."`
+    "body": "Honestly, I barely have the time to feed my cat."
   },
   {
     "character": ['Mayor Milford'],
-    "body": `"Anybody moves and I’ll blast her into kingdom come! And the hippie too!"`
+    "body": "Anybody moves and I’ll blast her into kingdom come! And the hippie too!"
   },
   {
     "character": ['Log Lady'],
-    "body": `"When this kind of fire starts, it is very hard to put out.
-    The tender boughs of innocence burn first, and the wind rises, and then all goodness is in jeopardy."`
+    "body": "When this kind of fire starts, it is very hard to put out. The tender boughs of innocence burn first, and the wind rises, and then all goodness is in jeopardy."
   },
   {
     "character": ['Audrey Horne'],
-    "body": `"I love this music. Isn’t it too dreamy?"`
+    "body": "I love this music. Isn’t it too dreamy?"
   },
   {
     "character": ['Nadine Hurley'],
-    "body": `"At high school, I used to watch Norma and you at those football games?
-    She was so pretty. You were such a handsome couple, but I knew, I always knew once you got to know me that we’d be together. Even though I was just a little nobody, a little brown mouse, I was always hoping. And wasn’t I right? Wasn’t I right?"`
+    "body": "At high school, I used to watch Norma and you at those football games? She was so pretty. You were such a handsome couple, but I knew, I always knew once you got to know me that we’d be together. Even though I was just a little nobody, a little brown mouse, I was always hoping. And wasn’t I right? Wasn’t I right?"
   },
   {
     "character": ['Ed Hurley'],
-    "body": `"I’m living my life, Norma. I just don’t like it much."`
+    "body": "I’m living my life, Norma. I just don’t like it much."
   },
   {
     "character": ['Agent Rosenfield'],
-    "body": `"The letter B, from Ronette’s finger, was cut from a copy of “Flesh World”.
-    Perfect match. This particular edition features swingers’ clubs for standard poodle enthusiasts. No comment."`
+    "body": "The letter B, from Ronette’s finger, was cut from a copy of 'Flesh World'. Perfect match. This particular edition features swingers’ clubs for standard poodle enthusiasts. No comment."
   },
   {
     "character": ['Deputy Director Gordon Cole'],
-    "body": `"COOPER, YOU REMIND ME TODAY OF A SMALL MEXICAN CHIHUAHUA."`
+    "body": "COOPER, YOU REMIND ME TODAY OF A SMALL MEXICAN CHIHUAHUA."
   },
   {
     "character": ['Sheriff Truman'],
-    "body": `"You know, you are the best lawman I’ve ever seen. But, Coop, sometimes you think too much."`
+    "body": "You know, you are the best lawman I’ve ever seen. But, Coop, sometimes you think too much."
   },
 ];
 
