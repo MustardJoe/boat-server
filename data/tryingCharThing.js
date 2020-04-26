c7f4c9a9f90fdaf123a54d92837ef805e82bf6e0
Jonathan Forney <jonathan.forney@gmail.com>
2020-04-25 23:47:29 -0700

    next batch is formatted

--- data/tryingCharThing.js
+++ data/tryingCharThing.js
@@ -1,93 +1,93 @@
 /*eslint quotes: ["error", "double", { "avoidEscape": true }]*/
 
 const chars = [
-  95: {
-    character: ['Leo Johnson'],
-    body: `"You punks owe me ten grand. Leo needs a new pair of shoes!"`
+  {
+    "character": ["5e9a6f9d2670900017421c4a"],
+    "body": "You punks owe me ten grand. Leo needs a new pair of shoes!"
   },
-  96: {
-    character: ['Ben Horne'],
-    body: `"Laura died yesterday. I lost you years ago."`
+  {
+    "character": ["5e9a77c82670900017421c4d"],
+    "body": "Laura died yesterday. I lost you years ago."
   },
-  97: {
-    character: ['Ben Horne'],
-    body: `"If you ever pull another stunt like that, you are going to be scrubbing bidets in a Bulgarian convent."`
+  {
+    "character": ["5e9a77c82670900017421c4d"],
+    "body": "If you ever pull another stunt like that, you are going to be scrubbing bidets in a Bulgarian convent."
   },
-  98: {
-    character: ['Ben Horne'],
-    body: `"I’m going to go give ‘little Elvis’ a bath."`
+  {
+    "character": ["5e9a77c82670900017421c4d"],
+    "body": "I’m going to go give ‘little Elvis’ a bath."
   },
-  99: {
-    character: ['Ben Horne'],
-    body: `"Agent Cooper. Let me give you the best advice that you’re gonna get all week. Men fall under the spell of Audrey’s charms like ducks in a shooting gallery."`
+  {
+    "character": ["5e9a77c82670900017421c4d"],
+    "body": "Agent Cooper. Let me give you the best advice that you’re gonna get all week. Men fall under the spell of Audrey’s charms like ducks in a shooting gallery."
   },
-  100: {
-    character: ['Ben Horne'],
-    body: `"Would you like us to hum? A Tibetan chant, perhaps?"`
+  {
+    "character": ["5e9a77c82670900017421c4d"],
+    "body": "Would you like us to hum? A Tibetan chant, perhaps?"
   },
-  101: {
-    character: ['Ben Horne'],
-    body: `"Sometimes the urge to do bad is nearly overpowering."`
+  {
+    "character": ["5e9a77c82670900017421c4d"],
+    "body": "Sometimes the urge to do bad is nearly overpowering."
   },
-  102: {
-    character: ['Josie Packard'],
-    body: `"Pete, I want to thank you for yesterday, for standing up to me with Catherine."`
+  {
+    "character": ["5e9b709bb042e100178ad4f9"],
+    "body": "Pete, I want to thank you for yesterday, for standing up to me with Catherine."
   },
-  103: {
-    character: ['Josie Packard'],
-    body: `"They want to hurt me. I know they do. Something horrible is going to happen, Harry."`
+  {
+    "character": ["5e9b709bb042e100178ad4f9"],
+    "body": "They want to hurt me. I know they do. Something horrible is going to happen, Harry."
   },
-  104: {
-    character: ['Dr. Jacoby'],
-    body: `"The problems of our entire society are of a sexual nature."`
+  {
+    "character": ["5e9b7669b042e100178ad4fa"],
+    "body": "The problems of our entire society are of a sexual nature."
   },
-  105: {
-    character: ['Dr. Jacoby'],
-    body: `"I’m a terrible person agent Cooper."`
+  {
+    "character": ["5e9b7669b042e100178ad4fa"],
+    "body": "I’m a terrible person agent Cooper."
   },
-  106: {
-    character: ['Dr. Jacoby'],
-    body: `"Let’s not talk about you or your parents anymore. Let’s talk about Laura."`
+  {
+    "character": ["5e9b7669b042e100178ad4fa"],
+    "body": "Let’s not talk about you or your parents anymore. Let’s talk about Laura."
   },
-  107: {
-    character: ['Dr. Jacoby'],
-    body: `"It was her, you see, the necklace: A divided heart. Laura was… – was in fact, well she was leading a double life. Two people."`
+  {
+    "character": ["5e9b7669b042e100178ad4fa"],
+    "body": "It was her, you see, the necklace: A divided heart. Laura was… – was in fact, well she was leading a double life. Two people."
   },
-  108: {
-    character: ['Dr. Jacoby'],
-    body: `"It’s like a… like a dream. There was a smell. Yeah there was a peculiar smell… This was a smell like… like Oil. Scorched engine oil."`
+  {
+    "character": ["5e9b7669b042e100178ad4fa"],
+    "body": "It’s like a… like a dream. There was a smell. Yeah there was a peculiar smell… This was a smell like… like Oil. Scorched engine oil."
   },
-  109: {
-    character: ['Nadine Hurley'],
-    body: `"Ed, are you waiting for those drapes to hang themselves?"`
+  {
+    "character": ["5e9b7ed8b042e100178ad4fc"],
+    "body": "Ed, are you waiting for those drapes to hang themselves?"
   },
-  110: {
-    character: ['Nadine Hurley'],
-    body: `"I’ve gotten all new drapes for my house. Ed bought’em for me yesterday at Gentleman Jim’s and we installed them together."`
+  {
+    "character": ["5e9b7ed8b042e100178ad4fc"],
+    "body": "I’ve gotten all new drapes for my house. Ed bought’em for me yesterday at Gentleman Jim’s and we installed them together."
   },
-  111: {
-    character: ['Nadine Hurley'],
-    body: `"Cotton balls. By God, those things will be quiet now."`
+  {
+    "character": ["5e9b7ed8b042e100178ad4fc"],
+    "body": "Cotton balls. By God, those things will be quiet now."
   },
-  112: {
-    character: ['Nadine Hurley'],
-    body: `"Doc Hayward says tonsillitis or no tonsillitis I can go to cheerleader try outs."`
+  {
+    "character": ["5e9b7ed8b042e100178ad4fc"],
+    "body": "Doc Hayward says tonsillitis or no tonsillitis I can go to cheerleader try outs."
   },
-  113: {
-    character: ['Nadine Hurley'],
-    body: `"I’m going to have the world’s first one hundred percent quiet runner."`
+  {
+    "character": ["5e9b7ed8b042e100178ad4fc"],
+    "body": "I’m going to have the world’s first one hundred percent quiet runner."
   },
-  114: {
-    character: ['Nadine Hurley'],
-    body: `"It is neat having the place all to ourselves. Pretend it’s ours, like we’re married or something!"`
+  {
+    "character": ["5e9b7ed8b042e100178ad4fc"],
+    "body": "It is neat having the place all to ourselves. Pretend it’s ours, like we’re married or something!"
   },
-  115: {
-    character: ['Ed Hurley'],
-    body: `"I sure know how to pick ‘m too."`
+  {
+    "character": ["5e9b8020b042e100178ad4fd"],
+    "body": "I sure know how to pick ‘m too."
   },
-  116: {
-    character: ['Ed Hurley'],
-    body: `"It’s not the first time, it won’t be the last, but I’m in that doghouse again."`
+  {
+    "character": ["5e9b8020b042e100178ad4fd"],
+    "body": "It’s not the first time, it won’t be the last, but I’m in that doghouse again."
   }
 ];
 
