56f46eadd11bc4ea3fc4c135fc031280ab87c8f6
Jonathan Forney <jonathan.forney@gmail.com>
2020-05-11 00:57:56 -0700

    cleaning things up a bit

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Started on ${PORT}`);
+  console.log(`Boat-Server now running on ${PORT}. Happy Computing!`);
 });
